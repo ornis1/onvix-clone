92d757b6d567bcffef6256f5da2feb40b4a8d17a
marat <fastmarat23@gmail.com>
2019-03-25 21:14:10 +0300

    add svg icons as vue components

---
+++ src/components/icons/IconReplay.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    class="sprite-svg sprite-svg--replay"
+    width="23px"
+    height="23px"
+    viewBox="0 0 23 23"
+  >
+    <path
+      opacity="1"
+      fill="currentColor"
+      fill-rule="evenodd"
+      d="M11.5 23C5.14872538 23 0 17.8512746 0 11.5 0 5.14872538 5.14872538 0 11.5 0 17.8512746 0 23 5.14872538 23 11.5 23 17.8512746 17.8512746 23 11.5 23zM9 6v11l8-5.522174L9 6z"
+    ></path>
+  </svg>
+</template>
